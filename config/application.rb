5ee94f218c981d3a5bce74ebd2e2ff65358defa9
arsley <blue20will@gmail.com>
2019-09-18 12:49:25 +0900

    :wrench: Change generator's workflow not to generate assets and helpers

--- config/application.rb
+++ config/application.rb
@@ -24,9 +24,9 @@ module Wariko
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
