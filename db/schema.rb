42ffc96f0d613c97a751d4cb2b713f354397d70c
arsley <blue20will@gmail.com>
2019-10-08 05:46:11 +0900

    :art: Rename column of users from username to email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_22_133500) do
+ActiveRecord::Schema.define(version: 2019_10_07_204425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2019_09_22_133500) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "email"
     t.string "nickname"
     t.string "password_digest"
     t.bigint "partner_id"
